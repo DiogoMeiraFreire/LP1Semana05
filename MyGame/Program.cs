d3181be77bc2400138644161622b3f236ea5611b
DiogoMeiraFreire <didi9934@gmail.com>
2024-03-22 23:31:20 +0000

    Added the powerup essentials to program (ex 12, i'm too sick to explain better)

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -6,32 +6,50 @@ namespace MyGame
     {
         static void Main(string[] args)
         {
-            // Accept argument and convert srt to int
             int numberOfVillains = int.Parse(args[0]); 
 
-            // array of enemies & accepts a number of enemies
             Enemy[] villains = new Enemy[numberOfVillains];
 
 
-            // loop through enemies 
             for (int i = 0; i < villains.Length; i++)
             {
-                // Request input => name of enemy
                 Console.Write($"Nome do inimigo {i+1}: ");
 
-                // store input
                 string name = Console.ReadLine();
 
-                // store info in enemy index position & create enemy
                 villains[i] = new Enemy(name);
 
             }
 
-            // just to print info
             foreach(Enemy enemy in villains)
             {
                 Console.WriteLine
                 ($"{enemy.GetName()} {enemy.GetHealth()} {enemy.GetShield()}"); 
+            }
+
+            villains[0].TakeDamage(60);
+
+            villains[0].PickupPowerUp(PowerUp.Health, 10);
+
+            villains[0].PickupPowerUp(PowerUp.Shield, 15);
+
+            villains[0].PickupPowerUp(PowerUp.Health, 10);
+
+
+            if(villains.Length > 1)
+            {
+                villains[1].TakeDamage(60);
+
+                villains[1].PickupPowerUp(PowerUp.Shield, 15);
+
+            }
+
+            
+            for(int i = 0; i < villains.Length; i++)
+            {
+                Console.WriteLine
+                ($"{villains[i].GetName()} {villains[i].GetHealth()}"
+                + $" {villains[i].GetShield()} {Enemy.GetPowerUpsCollected()}"); 
             }
         }
     }
