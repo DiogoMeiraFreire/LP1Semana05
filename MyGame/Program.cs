fb7eb4b1cf3d9fca37f1e7ad701f698769bffd36
DiogoMeiraFreire <didi9934@gmail.com>
2024-03-22 23:18:54 +0000

    Added the creation of enemies in the Main method

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -6,7 +6,33 @@ namespace MyGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Accept argument and convert srt to int
+            int numberOfVillains = int.Parse(args[0]); 
+
+            // array of enemies & accepts a number of enemies
+            Enemy[] villains = new Enemy[numberOfVillains];
+
+
+            // loop through enemies 
+            for (int i = 0; i < villains.Length; i++)
+            {
+                // Request input => name of enemy
+                Console.Write($"Nome do inimigo {i+1}: ");
+
+                // store input
+                string name = Console.ReadLine();
+
+                // store info in enemy index position & create enemy
+                villains[i] = new Enemy(name);
+
+            }
+
+            // just to print info
+            foreach(Enemy enemy in villains)
+            {
+                Console.WriteLine
+                ($"{enemy.GetName()} {enemy.GetHealth()} {enemy.GetShield()}"); 
+            }
         }
     }
 }
