379945a049d7acbf5ad71b6a774e1dee7a41d9f2
DiogoMeiraFreire <didi9934@gmail.com>
2024-03-22 22:46:44 +0000

    Added Crono1 to Chronos

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Diagnostics;
+
+// must use thread
+using System.Threading; 
 
 namespace Chronos
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Stopwatch crono1 = new Stopwatch(); 
+            
+            crono1.Start();
+
+            Thread.Sleep(600);
+
+            crono1.Stop();
+
+            float time1 = crono1.ElapsedMilliseconds / 1000f;
+
+            Console.WriteLine($"{time1:f3}"); 
+
         }
     }
 }
