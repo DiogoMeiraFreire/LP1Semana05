8e2770dbe79b864762375d1336bd18aecc8cba48
DiogoMeiraFreire <didi9934@gmail.com>
2024-03-22 22:47:18 +0000

    Added Crono2 to Chronos

--- Chronos/Program.cs
+++ Chronos/Program.cs
@@ -11,16 +11,25 @@ namespace Chronos
         private static void Main(string[] args)
         {
             Stopwatch crono1 = new Stopwatch(); 
-            
+            Stopwatch crono2 = new Stopwatch();
+
             crono1.Start();
 
             Thread.Sleep(600);
 
+            crono2.Start(); 
+
+            Thread.Sleep(200);
+
             crono1.Stop();
+            crono2.Stop();
 
             float time1 = crono1.ElapsedMilliseconds / 1000f;
+            float time2 = crono2.ElapsedMilliseconds / 1000f;
 
             Console.WriteLine($"{time1:f3}"); 
+
+            Console.WriteLine($"{time2:f3}"); 
 
         }
     }
